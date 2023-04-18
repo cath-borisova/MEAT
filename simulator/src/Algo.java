fe67e5a0fae0fdd3dbe81bbbdec38c0ec725108f
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2023-04-18 17:28:06 -0500

    moved coords out

--- simulator/src/Algo.java
+++ simulator/src/Algo.java
@@ -1,12 +1,4 @@
 package simulator.src;
 public interface Algo {
-    public class Coords{
-        int x;
-        int y;
-        public Coords(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int [] nextMove(int [] grid, int curX, int curY, int desX, int desY);
 }
---
+++ simulator/src/Coords.java
@@ -0,0 +1,10 @@
+package simulator.src;
+
+public class Coords {
+    int x;
+    int y;
+    public Coords(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
