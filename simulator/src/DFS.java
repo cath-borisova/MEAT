0fcda57be531ffc1348e03535569a115ade6a947
cbbor <c.b.borisova@gmail.com>
2023-04-18 17:29:15 -0500

    interface error

--- simulator/src/DFS.java
+++ simulator/src/DFS.java
@@ -1,6 +1,6 @@
 package simulator.src;
 public class DFS implements Algo {
-    public int [] NextMove(int [] grid, int curX, int curY, int desX, int desY){
+    public int [] nextMove(int [] grid, int curX, int curY, int desX, int desY){
         int [] coords = new int[2];
         return coords;
     }
